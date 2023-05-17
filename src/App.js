2ab6c74eb66dc7f48ae2eaae0e2877daa3bf0e9b
Natali <natalis13031987@gmail.com>
2023-05-17 15:17:00 +0300

    Add links

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather APP</div>;
+  return (
+    <div className="App">
+      <h1>Weather APP</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https://app.netlify.com/sites/fabulous-marzipan-645376/overview"
+          target="_blank"
+        >
+          Natalia Sukova{" "}
+        </a>
+        <a
+          href="https://github.com/NataliSfromUA/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sourced at Github
+        </a>
+      </footer>
+    </div>
+  );
 }
