822f6b8746b3c2ab534dc3b19dc0df7aa116c1bc
Natali <natalis13031987@gmail.com>
2023-05-17 16:35:58 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather APP</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https://app.netlify.com/sites/fabulous-marzipan-645376/overview"
-          target="_blank"
-        >
-          Natalia Sukova{" "}
-        </a>
-        <a
-          href="https://github.com/NataliSfromUA/react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced at Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather APP</h1>
+        <footer>
+          This project was coded by
+          <a
+            href="https://app.netlify.com/sites/fabulous-marzipan-645376/overview"
+            target="_blank"
+          >
+            {" "}
+            Natalia Sukova{" "}
+          </a>{" "}
+          open-sourced at
+          <a
+            href="https://github.com/NataliSfromUA/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
